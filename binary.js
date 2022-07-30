e4abf0281c3dbbf6b7500ac95ab8489cb75d225b
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-07-29 20:16:45 -0400

    binary week 10

--- binary.js
+++ binary.js
@@ -1,22 +1,32 @@
 
 
 
-
-
 function convertToBinary1(n) {
-  let binary = n.toString(2);  // change the number to binary 
-    let bits =  binary.split(0) // split the bits at 0
-    let sortedBits = bits.reduce((a,b)=> a > b ? a:b ); // sort the bits and we get the one with most 1's
+    let binary = n.toString(2);  // change the number to binary 
+      let bits =  binary.split(0) // split the bits at 0
+      let sortedBits = bits.reduce((a,b)=> a > b ? a:b ); // sort the bits and we get the one with most 1's
+  
+   console.log(sortedBits.length)
+  
+    }
+  
+  
 
- console.log(sortedBits.length)
+// function convertToBinary1(number) {
+//   let binary = number.toString(2);
+//   let arr = [];
+//   let counter = 1;
 
-  }
+//   for (let i = 0; i <= binary.length; i++) {
+//     if (binary[i] ==  1 && binary[i+1] == 1) {
+//       arr.push(binary[i])
+//       counter++
+//     }
+//   console.log(counter)
+//   console.log(arr)
+//   }
+// }
 
 
 
-
-
-
-
-
-convertToBinary1(6);
+// convertToBinary1(6);
